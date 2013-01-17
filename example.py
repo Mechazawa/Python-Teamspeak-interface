4580563378663e85cf978b3add7eaef1c7a0f87d
Mega1mpact <BasBieling@gmail.com>
2013-01-17 15:58:31 +0100

    Fixed the example to work with the new changes

--- example.py
+++ example.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
 import Teamspeak
 import sys
 
 # Create a new instance that will connect to localhost
 print 'Connecting...'
-ts3 = Teamspeak.TeamSpeak()
+ts3 = Teamspeak.TeamSpeak() # It will connect to localhost on port 10011 (default query port)
 
 # Connect to the server
 try:
@@ -12,16 +13,15 @@ except Exception, e: # An exception is thrown if something goes wrong
     print e
 
 # Check if we're connected
-if not ts3.connected:
+if not ts3.connected():
     print "Oops something went wrong."
     sys.exit(1)
-
+print 'Connected: ',ts3.connected()
 print 'Sending a message'
 # Let's encode the arguments
 args = ts3.encode({
-                    'targetmode':2,
-                    'target':1,
-                    'msg': 'Hello world'
+                    'targetmode' : 2,
+                    'msg' : 'Hello world'
                 })
 # Now we're going to send a message and then print out
 #   what the server returns
@@ -29,7 +29,7 @@ print ts3.decode(ts3.sendCommand('sendtextmessage '+args))
 
 # That was fun now let's listen into a conversation
 
-print 'Stalking'
+print 'Stalking...'
 # Define the callback
 def eventcallback(ts3, raw):
     message = ts3.decode(raw)
@@ -38,9 +38,7 @@ def eventcallback(ts3, raw):
     if message['msg'] == 'go away bot':
         return False
 # Register the listener
-ts3.registerEvent(eventcallback, event='textchannel', cid=1)
+ts3.registerEvent(callback=eventcallback, channel=1)
 
 # Exit the program
 print 'I\'m all done now'
-
-
